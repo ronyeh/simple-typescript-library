ae1624a8c0a862c9c5b09c82e392473f1e536ca5
Ron B. Yeh <ronyeh@gmail.com>
2022-01-17 13:49:33 -0800

    Remove dist/ after releasing version 0.1.10-alpha.0.

--- dist/a.test.js
+++
@@ -1,4 +0,0 @@
-import { A } from "./a.js";
-test("A's getValue() returns 1", () => {
-    expect(new A().getValue()).toBe(1);
-});
--- dist/version.js
+++
@@ -1,3 +0,0 @@
-export const VERSION = '0.1.10-alpha.0';
-export const ID = 'd9c0c440555e0a84bbdff36c62c4cdabd4c3802d';
-export const DATE = '2022-01-17T21:49:11.762Z';
