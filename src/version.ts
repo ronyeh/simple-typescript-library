0fb8be8ae592325d9c9e936f65f02e83e097fee2
Ron B. Yeh <ronyeh@gmail.com>
2022-01-17 13:50:32 -0800

    Remove the preversion, version, postversion scripts from package.json.         "preversion": "npm run grunt release:preversion",         "version": "npm run grunt release:version",         "postversion": "npm run grunt release:postversion"

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const VERSION = '0.1.9';
-export const ID = '4c8678a6d0d776f6169d0eda2a5c8a677ed62e5c';
-export const DATE = '2022-01-17T10:06:19.777Z';
+export const VERSION = '0.1.10-alpha.0';
+export const ID = 'd9c0c440555e0a84bbdff36c62c4cdabd4c3802d';
+export const DATE = '2022-01-17T21:49:11.762Z';
