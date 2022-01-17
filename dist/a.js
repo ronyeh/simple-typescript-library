936107d30c4b18e220456b22524676923b5e12e7
Ron B. Yeh <ronyeh@gmail.com>
2022-01-17 13:49:16 -0800

    Bump version to 0.1.10-alpha.0

---
+++ dist/a.js
@@ -0,0 +1,12 @@
+class A {
+    constructor() {
+        console.log("new A()");
+    }
+    doSomething() {
+        console.log("I am of type [A]");
+    }
+    getValue() {
+        return 1;
+    }
+}
+export { A };
