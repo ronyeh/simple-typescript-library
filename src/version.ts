74ac82656622e96b5378dfdbbe43647be641d37a
Ron B. Yeh <ronyeh@gmail.com>
2022-01-18 02:07:14 -0800

    Update release-it dependency.

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const VERSION = '0.1.10-rc.5';
-export const ID = '5ffba24bd809c9a12529081930939c6cc18f152d';
-export const DATE = '2022-01-18T01:54:54.492Z';
+export const VERSION = '0.2.0-alpha.2';
+export const ID = '442211cc4e59c1c17b31281f41956604e93980d4';
+export const DATE = '2022-01-18T05:52:49.800Z';
