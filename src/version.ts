02f6be5a29c90f9dccc853e964e8c2b248e6a4b2
Ron B. Yeh <ronyeh@gmail.com>
2022-01-18 02:21:02 -0800

    Release simple-typescript-library 0.2.2-rc.0

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const VERSION = '0.2.2-beta.0';
-export const ID = '84edac79a78d3c00c6cb1edc13e1c96866959062';
-export const DATE = '2022-01-18T10:19:53.380Z';
+export const VERSION = '0.2.2-rc.0';
+export const ID = '5477c3086209c027955f871663fa9de2eeb73903';
+export const DATE = '2022-01-18T10:20:43.240Z';
